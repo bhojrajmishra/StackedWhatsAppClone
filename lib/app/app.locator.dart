efefcf0eefb27f427dba4eb054586b7c13f971df
Bhoj Raj Mishra <bhojrajmishra4@gmail.com>
2024-09-08 13:55:59 +0545

    fix: fix the flutter secure storage

--- lib/app/app.locator.dart
+++ lib/app/app.locator.dart
@@ -6,12 +6,12 @@
 
 // ignore_for_file: public_member_api_docs, implementation_imports, depend_on_referenced_packages
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:stacked_services/src/bottom_sheet/bottom_sheet_service.dart';
 import 'package:stacked_services/src/dialog/dialog_service.dart';
 import 'package:stacked_services/src/navigation/navigation_service.dart';
 import 'package:stacked_services/src/snackbar/snackbar_service.dart';
 import 'package:stacked_shared/stacked_shared.dart';
-
 import '../services/home_service.dart';
 import '../services/login_service.dart';
 import '../services/registration_service.dart';
@@ -34,4 +34,5 @@ Future<void> setupLocator({
   locator.registerLazySingleton(() => HomeService());
   locator.registerLazySingleton(() => RegistrationService());
   locator.registerLazySingleton(() => SnackbarService());
+  locator.registerLazySingleton(() => const FlutterSecureStorage());
 }
--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:stacked/stacked.dart';
 import 'package:whats_app_clone/app/app.locator.dart';
 import 'package:whats_app_clone/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
-import 'package:whats_app_clone/local_storage/storage_service.dart';
 
 class StartupViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
-  final StorageService _storage = StorageService();
+  final _storage = locator<FlutterSecureStorage>();
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
     // You can check for the user token here and decide where to navigate
